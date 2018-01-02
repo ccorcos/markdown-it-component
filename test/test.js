c1259aa587ef44db50d1a49d83f1503e045c8962
Chet Corcos <ccorcos@gmail.com>
2018-01-02 13:29:06 -0800

    Tests working

--- test/test.js
+++ test/test.js
@@ -5,3 +5,10 @@ describe("markdown-it-sup", function() {
 	var md = require("markdown-it")({ xhtmlOut: true }).use(require("../")())
 	generate(path.join(__dirname, "fixtures/component.txt"), md)
 })
+
+describe("markdown-it-sup2", function() {
+	var md = require("markdown-it")({ xhtmlOut: true }).use(
+		require("../")({ jsonData: true })
+	)
+	generate(path.join(__dirname, "fixtures/component2.txt"), md)
+})
